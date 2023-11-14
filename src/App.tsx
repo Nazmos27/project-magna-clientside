3c0c76ef613cb42c4b1c175cae3cb2bb6643bc58
Nazmos27 <nazmos789@gmail.com>
2023-11-14 23:54:41 +0600

    added materialUI and tested with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <h1 className='text-2xl'>Hey Hello world after a long time</h1>
+      <Button variant='contained' className='text-2xl bg-slate-500'>Hello MaterialUi</Button>
     </>
   )
 }
 
-export default App
+export default App;
