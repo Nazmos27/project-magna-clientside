b1a2c5fa7c9672bed43227477814b87abfd40479
Nazmos27 <nazmos789@gmail.com>
2023-11-17 23:59:05 +0600

    motion compo added

--- src/Components/Home/HomePage.tsx
+++ src/Components/Home/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BannerSection from './BannerSection'
 import PostsSection from './PostsSection'
+import SideMenu from './SideMenu'
 
 const HomePage = () => {
   return (
     <>
     <BannerSection></BannerSection>
     <PostsSection></PostsSection>
+    <SideMenu></SideMenu>
     </>
   )
 }
