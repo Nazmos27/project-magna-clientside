b23b8bd250099dcfa53fa711ece89d40467524ac
Nazmos27 <nazmos789@gmail.com>
2023-11-26 23:33:12 +0600

    about us compo added

---
+++ src/Components/Home/AboutUs.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <h1 className='text-3xl'>About Us</h1>
+        <p className='text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur exercitationem unde pariatur quibusdam dolorem nulla quod voluptatum, iure officiis dolorum id, adipisci corrupti odit numquam suscipit laborum. Culpa, sint.</p>
+    </div>
+  )
+}
+
+export default AboutUs
