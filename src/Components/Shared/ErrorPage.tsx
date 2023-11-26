36cd5068b94dd10bda6d51c430ac73a02db8e173
Nazmos27 <nazmos789@gmail.com>
2023-11-26 23:41:35 +0600

    error page modified

--- src/Components/Shared/ErrorPage.tsx
+++ src/Components/Shared/ErrorPage.tsx
@@ -4,6 +4,7 @@ const ErrorPage = () => {
   return (
     <div>
         <h1 className='text-4xl text-red-600'>404 not found</h1>
+        <img src="../../../public/errorPage.gif" alt="" />
     </div>
   )
 }
