01e57656dc417cfac41b1ada28ad7531af465a8b
Nazmos27 <nazmos789@gmail.com>
2023-12-06 23:43:36 +0600

    Making chatbox UI

--- src/Components/Dashboards/User/MessageComponents/MyChat.tsx
+++ src/Components/Dashboards/User/MessageComponents/MyChat.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import avatar from '../../../../../public/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.webp'
+import { Phone, PhoneEnabledSharp } from '@mui/icons-material'
 
 const MyChat = () => {
 
@@ -43,21 +44,21 @@ const MyChat = () => {
 
   return (
     <div className='w-screen flex'>
-      <div className='w-1/4 h-screen bg-gray-200 shadow-lg  overflow-auto'>
+
+      {/* Messages Section */}
+      <div className='w-1/4 h-screen bg-gray-200 shadow-lg' >
         <div className='sticky bg-gray-200 top-0 pt-3'>
           <div className='flex justify-center items-center my-4'>
-            <img src={avatar} className='h-16 w-16 rounded-full border-green-500 p-1 border-2' alt="" />
+            <img src={avatar} className='h-12 w-12 rounded-full border-green-500 p-1 border-2' alt="" />
             <div className='text-left ml-2'>
-              <h1 className='font-semibold text-xl'>Nazmos Sakib</h1>
-              <p>My Account</p>
+              <h1 className='font-semibold'>Nazmos Sakib</h1>
+              <p className='text-sm'>My Account</p>
             </div>
           </div>
           <hr className='border-[1px] border-gray-400' />
         </div>
-
-
-        <section className=''>
-          <div className='flex flex-col items-start ml-6'>
+        <section className='h-[80%] overflow-y-scroll' >
+          <div className='h-[1000px] flex flex-col items-start ml-6' >
             {
               contact.map(({ name, img, status }) => {
                 return (
@@ -74,7 +75,30 @@ const MyChat = () => {
           </div>
         </section>
       </div>
-      <div className='w-1/2 h-screen bg-gray-200 shadow-lg'>hey</div>
+
+      {/* chatting section */}
+
+      <div className='w-1/2 h-screen bg-white shadow-lg'>
+        
+          {/* header part */}
+          <div className='bg-gray-100 rounded-full mx-5 mb-5 flex justify-between px-10 items-center'>
+            <div className='flex justify-center items-center my-4'>
+              <img src={avatar} className='h-12 w-12 rounded-full border-green-500 p-1 border-2' alt="" />
+              <div className='text-left ml-2'>
+                <h1 className='font-semibold'>Nazmos Sakib</h1>
+                <p className='text-sm'>My Account</p>
+              </div>
+            </div>
+            <PhoneEnabledSharp />
+          </div>
+          <div className='h-[75%] border overflow-y-scroll border-black shadow-md'>
+            <div className='h-[1000px] px-10 py-14'>
+              <div className='bg-slate-200 text-sm p-2 my-4 w-[300px] rounded-b-xl rounded-tr-xl'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quam accusamus voluptatem aliquid. Error modi labore iure doloribus incidunt ab!</div>
+              <div className='bg-[#00B2FF] text-sm p-2 my-4 w-[300px] ml-auto text-white rounded-b-xl rounded-tl-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quam accusamus voluptatem aliquid. Error modi labore iure doloribus incidunt ab!</div>
+            </div>
+          </div>
+        </div>
+      
       <div className='w-1/4 h-screen bg-gray-200 shadow-lg'>hey</div>
     </div>
   )
